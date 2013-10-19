df90297272c2f9d1b2f9ec964f3ba80c7c2a7539
Q <life.qm@gmail.com>
2013-10-19 08:39:38 -0700

    add test service

---
+++ q-rpc/include/test_service.h
@@ -0,0 +1,38 @@
+
+#ifndef test_service_h_
+#define test_service_h_
+
+
+namespace rpc { namespace services {
+
+class test_service 
+: public rpc::services::iservice
+{
+
+// connection point 
+connection_impl(test_service)
+
+// rpc interfaces
+rpc_methods_begin(test_service)
+rpc_method(1, add)
+rpc_method(2, test)
+rpc_methods_end()
+
+// ½Ó¿Ú
+public:
+  void add(rpc::protocol::request& req, rpc::protocol::response& res, rpc::server_session_ptr) {
+    res.body()["data"] = req.params()["p0"].asInt() + req.params()["p1"].asInt();
+  }
+
+  void test(rpc::protocol::request&, rpc::protocol::response&, rpc::server_session_ptr) {
+    
+  }
+
+}; 
+
+
+} } // namespace services } namespace rpc
+
+#endif // test_service_h_
+
+
